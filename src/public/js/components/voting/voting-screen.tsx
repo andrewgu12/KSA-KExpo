cf917d8c176dd9c80c09ba2ec536877e97e4ef65
sanidjaya <sanidjaya@gmail.com>
2018-03-30 00:48:43 -0400

    changing class of heart?

--- src/public/js/components/voting/voting-screen.tsx
+++ src/public/js/components/voting/voting-screen.tsx
@@ -82,7 +82,7 @@ export default class VotingScreen extends React.Component<Props, State> {
           direction: direction
         }).then((res) => {
           this.setState({currentVote: newVote});
-          (direction == "increment") ? this.setState({heartClass: 'fas fa-heart'}) : this.setState({heartClass: 'far fa-heart'})
+          (currentVote) ? this.setState({heartClass: "fas fa-heart"}) : this.setState({heartClass: "far fa-heart"});
         }).catch((err) => {
           console.log(err);
         });
@@ -137,6 +137,7 @@ export default class VotingScreen extends React.Component<Props, State> {
             <button id="next-performance" onClick={this.updateCurrentPerformance}>
               <i className="fas fa-arrow-right nav-arrow"></i>     
             </button>
+            <p>{this.state.errorMessage}</p>
           </div>
         </div>
       </div>
